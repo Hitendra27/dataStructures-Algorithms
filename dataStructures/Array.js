6f94b0a7c9c1a665f90ed4f758d96c6c0226d33e
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-03-05 22:53:39 +0000

    Array Data Structure.

---
+++ dataStructures/Array.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 'Sanj', 5]
+arr.push(100)
+arr.unshift(0)
+arr.pop()
+arr.shift()
+
+
+for(const item of arr) {
+    console.log(item)
+
+}
+
+// map, filter, reduce, concat, slice and splice
