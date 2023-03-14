317d3f4c86fd48b295345e6b8b2f7feec649b126
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-03-14 22:28:27 +0000

    Added LinkedList DSA.

---
+++ dataStructures/LinkedList.js
@@ -0,0 +1,40 @@
+class Node{
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+    isEmpty() {
+        return this.size === 0
+    }
+
+    getSize() {
+        return this.size
+    }
+
+    prepand() {
+        const node = new Node(value)
+        if(this.isEmpty()) {
+            this.head = node
+        } else {
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+    }
+}
+
+
+const list = new LinkedList()
+console.log('List is empty? ', list.isEmpty())
+console.log('List size ', list.getSize())
+list.prepand(10)
+list.prepand(20)
+list.prepand(30)
